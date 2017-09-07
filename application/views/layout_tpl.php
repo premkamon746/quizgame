15cd1c434c26ee57496768a9d9e81a34d5538ade
premkamon <premkamon746@gmail.com>
2017-09-07 07:29:44 +0700

    update footer and css

--- application/views/layout_tpl.php
+++ application/views/layout_tpl.php
@@ -215,9 +215,41 @@
         }
       </script>
       <br/><br/><br/>
-      <p>
-      	  <a href="<?=site_url("/")?>">home</a>
-      	|<a href="<?=site_url("/content/policy")?>">Privacy Policy</a>
-      </p>
+
+      <footer class="footer">
+        <div class="container">
+            <div class="row">
+
+
+                 <div class="col-lg-3 col-sm-3">
+                    <address>
+                        <p><a href="<?=site_url("/")?>">Home</a></p>
+                        <p><a href="<?=site_url("/content/policy")?>">Privacy Policy</a></p>
+
+                    </address>
+                </div>
+                <div class="col-lg-5 col-sm-5">
+                   <!-- <h1>latest tweet</h1>
+                    <div class="tweet-box">
+                        <i class="icon-twitter"></i>
+                        <em>Please follow <a href="javascript:;">@nettus</a> for all future updates of us! <a href="javascript:;">twitter.com/vectorlab</a></em>
+                    </div>-->
+                </div>
+                <div class="col-lg-3 col-sm-3 col-lg-offset-1">
+                    <!--<h1>stay connected</h1>
+                    <ul class="social-link-footer list-unstyled">
+                        <li><a href="#"><i class="icon-facebook"></i></a></li>
+                        <li><a href="#"><i class="icon-google-plus"></i></a></li>
+                        <li><a href="#"><i class="icon-dribbble"></i></a></li>
+                        <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                        <li><a href="#"><i class="icon-twitter"></i></a></li>
+                        <li><a href="#"><i class="icon-skype"></i></a></li>
+                        <li><a href="#"><i class="icon-github"></i></a></li>
+                        <li><a href="#"><i class="icon-youtube"></i></a></li>
+                    </ul>-->
+                </div>
+            </div>
+        </div>
+    </footer>
   </body>
 </html>
